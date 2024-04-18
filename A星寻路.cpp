7a5c8fbc84484a4312e0929e49a0480ded851bac
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-04-18 13:22:30 +0800

    Update A星寻路.cpp
    Signed-off-by: BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>

--- A星寻路.cpp
+++ A星寻路.cpp
@@ -1,9 +1,10 @@
 #include "iostream"
 #include "vector"
 #include "cstring"
+#include "stack"
 
 using namespace std;
-
+//地图大小
 #define hang 10
 #define lie 8
 
@@ -17,8 +18,10 @@ const int map[lie][hang] = {{1, 1, 0, 0, 0, 1, 0, 0, 0, 0},
                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                             {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},};
 
-//位置信息
-struct point {
+
+//结构体A
+class A {
+public:
     //列位置
     int lienum;
     //行位置
@@ -29,34 +32,28 @@ struct point {
     int g;
     //估算代价
     int h;
-};
-
-//结构体A
-struct A {
-    //位置信息
-    point pos;
     //A的父
     A *parent;
 };
+
+//计算某点的H值
+int getH(const A start, const A end) {
+    int temphang = abs(start.hangnum - end.hangnum);
+    int templie = abs(start.lienum - end.lienum);
+    return temphang + templie;
+}
 
 //方向字典
 enum direction {
     you, zuo, shang, xia
 };
 
-//计算某点的H值
-int getH(const point start, const point end) {
-    int temphang = abs(start.hangnum - end.hangnum);
-    int templie = abs(start.lienum - end.lienum);
-    return temphang + templie;
-}
-
 //将A*指针扩容
 A *newA(const int lienum, const int hangnum) {
     A *p = new A;
     memset(p, 0, sizeof(A));
-    p->pos.lienum = lienum;
-    p->pos.hangnum = hangnum;
+    p->lienum = lienum;
+    p->hangnum = hangnum;
     return p;
 }
 
@@ -68,52 +65,50 @@ int main() {
     //找了几次
     int times = 0;
     //起点
-    point start{1, 1};
+    A start{1, 1};
     //终点
-    point end{6, 8};
+    A end{6, 8};
     //开始位置的A
     A *pCurrent = newA(start.lienum, start.hangnum);
     //开始位置标记找过
-    pathMap[pCurrent->pos.lienum][pCurrent->pos.hangnum] = 1;
+    pathMap[pCurrent->lienum][pCurrent->hangnum] = 1;
     //队列存储A
     vector<A *> list;
     //主程序
     while (true) {
         for (int i = 0; i < 4; i++) {
             //新建一个指针
-            A *pChild = newA(pCurrent->pos.lienum, pCurrent->pos.hangnum);
+            A *pChild = newA(pCurrent->lienum, pCurrent->hangnum);
             //保留G的值
-            pChild->pos.g = pCurrent->pos.g;
+            pChild->g = pCurrent->g;
             //根据四个方向调整坐标
             switch (i) {
                 case you:
-                    pChild->pos.lienum++;
+                    pChild->lienum++;
                     break;
                 case zuo:
-                    pChild->pos.lienum--;
+                    pChild->lienum--;
                     break;
                 case shang:
-                    pChild->pos.hangnum--;
+                    pChild->hangnum--;
                     break;
                 case xia:
-                    pChild->pos.hangnum++;
+                    pChild->hangnum++;
                     break;
                 default:
                     abort();
             }
             //G的值加1
-            pChild->pos.g++;
+            pChild->g++;
             //如果没越界，没障碍并且没找过
-            if (pChild->pos.lienum >= 0 && pChild->pos.lienum <= lie &&
-                pChild->pos.hangnum >= 0 && pChild->pos.hangnum <= hang &&
-                map[pChild->pos.lienum][pChild->pos.hangnum] != 1 &&
-                pathMap[pChild->pos.lienum][pChild->pos.hangnum] != 1) {
+            if (pChild->lienum >= 0 && pChild->lienum <= lie && pChild->hangnum >= 0 && pChild->hangnum <= hang &&
+                map[pChild->lienum][pChild->hangnum] != 1 && pathMap[pChild->lienum][pChild->hangnum] != 1) {
                 //标记为已找过
-                pathMap[pChild->pos.lienum][pChild->pos.hangnum] = 1;
+                pathMap[pChild->lienum][pChild->hangnum] = 1;
                 //计算H的值
-                pChild->pos.h = getH(pChild->pos, end);
+                pChild->h = getH(*pChild, end);
                 //计算F的值
-                pChild->pos.f = pChild->pos.h + pChild->pos.g;
+                pChild->f = pChild->h + pChild->g;
                 //标记父
                 pChild->parent = pCurrent;
                 //写入待处理列表
@@ -124,7 +119,7 @@ int main() {
         //处理列表
         auto imin = list.begin();
         for (auto i = list.begin(); i != list.end(); i++) {
-            if ((*imin)->pos.f > (*i)->pos.f) {
+            if ((*imin)->f > (*i)->f) {
                 imin = i;
             }
         }
@@ -133,7 +128,7 @@ int main() {
         //抹掉最小的
         list.erase(imin);
         //如果到达了终点
-        if (end.lienum == pCurrent->pos.lienum && end.hangnum == pCurrent->pos.hangnum) {
+        if (end.lienum == pCurrent->lienum && end.hangnum == pCurrent->hangnum) {
             find = true;
             break;
         }
@@ -145,11 +140,23 @@ int main() {
     }
     //如果找到了
     if (find) {
+        //输出
         cout << "找到终点了" << endl;
-        cout << "从后往前为：" << endl;
+        cout << "从前往后为：" << endl;
+        //存放路径的栈
+        stack<A *> show;
+        //输出路径到栈
         while (pCurrent) {
-            cout << "(" << pCurrent->pos.hangnum << "," << pCurrent->pos.lienum << ") ";
+            auto temp = newA(pCurrent->hangnum, pCurrent->lienum);
+            show.push(temp);
             pCurrent = pCurrent->parent;
+        }
+        delete pCurrent;
+        //出栈
+        while (!show.empty()) {
+            auto temp = show.top();
+            show.pop();
+            cout << "(" << temp->hangnum << "," << temp->lienum << ") ";
         }
     } else {
         cout << "没找到终点" << endl;
